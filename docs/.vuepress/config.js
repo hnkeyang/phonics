32fe3cfd25f0923c5c1b877ee96c95fb42ab6c4f
hnkeyang <hnkeyang@gmail.com>
2026-02-26 17:55:12 +0800

    nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,14 @@
 import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
 import { plumeTheme } from 'vuepress-theme-plume'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
 	bundler: viteBundler(),
 	base: '/phonics/',
-	theme: plumeTheme({
-		navbar: [
+	theme: defaultTheme({
+		//sidebar: ["/英语自然拼读法.md", "/phonetic.html", "/字母及字母组合发音规律.md"],
+		sidebar: [
 			{
 			text: "英语自然拼读法",
 			link: "/英语自然拼读法.md",
@@ -14,7 +16,8 @@ export default defineUserConfig({
 			// 仅在 `/zh/guide/` 激活
 			//activeMatch: "^/zh/guide/$",
 			},
-			{ text: "48个国际音标", link: "/phonics/phonetic.html", icon: "config" },
+			//{ text: "48个国际音标", link: "/phonics/phonetic.html", icon: "config" },
+			{ text: "48个国际音标", link: "/phonetic.html", icon: "config" },
 			{
 			text: "字母及字母组合发音规律",
 			link: "/字母及字母组合发音规律.md",
